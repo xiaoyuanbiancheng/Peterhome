e0591b40db2aa6ff95bf35192d709c6528843464
Perter Li <2986255126@qq.com>
2025-08-09 21:41:10 +0800

    前端编程学习
    最新html,css，js学习

---
+++ JS学习/变量与常量/index.js
@@ -0,0 +1,10 @@
+// 声明一个变量num，并且赋值为10
+//var num = 0;
+//num = 20; //重新赋值
+
+/**
+ * 打印，会在浏览器的控制台中显示出来
+ */
+
+const URL="http://www.itbaizhan.com";
+URL = "http://www.baidu.com";
---
+++ js源码/JS学习/数据类型/index.js
@@ -0,0 +1,31 @@
+// 数值
+var age = 10;
+var num = 21.5;
+var num1 = -20;
+
+//字符串
+var str = "hello";
+var name = "尚学堂";
+
+//布尔值
+var isTrue = true; //真 应用场景判断
+var isFalse = false;
+
+//undefined
+//null
+
+// 合成类型（复合类型） 对象
+/**
+ * 狭义对象和广义对象
+ * 狭义对象： 键值对集合
+ * 广义对象： 在JavaScript中，一切皆对象
+ */
+var obj = {
+    name: "itbazhan",
+    age: 110,
+    flag: false;
+
+}
+
+var hello; //undefied
+var timer = null; //初始化
